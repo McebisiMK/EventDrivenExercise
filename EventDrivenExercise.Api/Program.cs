fbeb5a5b7bb54136e5a0dc94f28b6c088cfc2b87
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-06-12 14:33:28 +0200

    Registered audit log service

--- EventDrivenExercise.Api/Program.cs
+++ EventDrivenExercise.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<EventDrivenDbContext>(options => options.UseSqlSer
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserAuditLogService, UserAuditLogService>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddControllers();
