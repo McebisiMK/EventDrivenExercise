713fb8f210768859c0f223453bad2d7c4d8ee05c
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-06-12 10:53:22 +0200

    Added user dto

---
+++ EventDrivenExercise.Common/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+namespace EventDrivenExercise.Common.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
